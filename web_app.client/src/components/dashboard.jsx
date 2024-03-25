b6c4489b0a6d98d4fc6137f085b84d94456c56dd
Kristiyan Stoyanov <kristiyan.stoyanov.krs@gmail.com>
2024-03-25 23:15:02 +0200

    added pokemon images to dashboard table

--- web_app.client/src/components/dashboard.jsx
+++ web_app.client/src/components/dashboard.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect, useCallback } from 'react';
 function Dashboard() {
     const [pokemonData, setPokemonData] = useState();
     const [requestURL, setRequestURL] = useState('https://pokeapi.co/api/v2/pokemon/')
+    const [imageRequestURL, setImageRequestURL] = useState('');
+    const [imageURL, setImageURL] = useState('');
 
     const getPokemonData = useCallback(async () => {
         var isSubscribed = true;
@@ -11,12 +13,24 @@ function Dashboard() {
         if (isSubscribed) {
             setPokemonData(data);
         }
+        data.results.map((pokemon) => setImageRequestURL(pokemon.url));
         return () => isSubscribed = false;
     }, [requestURL])
 
+    const getPokemonImage = useCallback(async () => {
+        var isSubscribed = true;
+        const response = await fetch(imageRequestURL);
+        const data = await response.json();
+        if (isSubscribed) {
+            setImageURL(data);
+        }
+        return () => isSubscribed = false;
+    }, [imageRequestURL])
+
     useEffect(() => {
         getPokemonData().catch(console.error);
-    }, [getPokemonData])
+        getPokemonImage().catch(console.error);
+    }, [getPokemonData, getPokemonImage])
 
     return (
         <div className="layout-wrapper">
@@ -25,17 +39,15 @@ function Dashboard() {
                 <thead>
                     <tr>
                         <th>NAME</th>
-                        <th>DESCRIPTION</th>
-                        <th>DESCRIPTION</th>
+                        <th>IMAGE</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {pokemonData.results.map((poke) =>
-                    <tr key={poke.name}>
-                        <td>{poke.name}</td>
-                        <td>description</td>
-                        <td>description</td>
-                        </tr>)}
+                    {pokemonData.results.map((pokemon) => (
+                    <tr key={pokemon.name}>
+                        <td>{pokemon.name}</td>
+                            <td>{imageURL ? <img src={imageURL.sprites.other.home.front_default} width={100} height={100} /> : <>Loading...</>}</td>
+                    </tr>) ) }
 
                     {/*this is a silly way of aligning the buttons but I am running out of time*/}
 
@@ -46,8 +58,6 @@ function Dashboard() {
                                 <button id="prevBtn" type="button" onClick={() =>
                                     setRequestURL(pokemonData.previous)} >Previous</button>
                             </td>
-                            <td>
-                            </td>
                             <td align="right">
                                 <button id="nextBtn" type="button" onClick={() =>
                                     setRequestURL(pokemonData.next)} >Next</button>
@@ -57,8 +67,6 @@ function Dashboard() {
                     {pokemonData.next &&
                         !pokemonData.previous &&
                         <>
-                        <td>
-                        </td>
                         <td>
                         </td>
                             <td align="right">
