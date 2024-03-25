a5011abb25d0a78daa391d88cf7295f6e0390bb5
Kristiyan Stoyanov <kristiyan.stoyanov.krs@gmail.com>
2024-03-25 19:13:44 +0200

    integrated profile page with pokemon api

--- web_app.client/src/components/dashboard.jsx
+++ web_app.client/src/components/dashboard.jsx
@@ -1,9 +1,40 @@
+import { useState, useEffect } from 'react';
+
 function Dashboard() {
+    const [pokemon, setPokemon] = useState();
+
+    useEffect(() => {
+        populatePokemonData();
+    }, [])
+
     return (
         <div className="layout-wrapper">
-            <h2>Dashboard</h2>
+            {pokemon &&
+                (<table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {pokemon.results.map((poke) =>
+                    <tr key={poke.name}>
+                        <td>{poke.name}</td>
+                    </tr>)}
+                </tbody>
+                <button type="button" >Next</button>
+                </table>
+                )
+            }
         </div>
     );
+
+    async function populatePokemonData() {
+        const response = await fetch(' https://pokeapi.co/api/v2/pokemon/');
+        const data = await response.json();
+        setPokemon(data);
+        console.log(data);
+    }
 }
 
 export default Dashboard;
